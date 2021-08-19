4ea3f5aad1d3880f005e5867d7b256fd153ada86
Michalis <PanayidesM@cardiff.ac.uk>
2021-08-19 15:06:43 +0100

    Apply chnages based on flake8 and pylint

--- src/pykemon_battle/battle.py
+++ src/pykemon_battle/battle.py
@@ -3,6 +3,7 @@ import time
 
 # from .move import Move
 from .pokemon import Pokemon
+from .utils import enemy_turn_logic, player_turn_logic
 
 
 class Battle:
@@ -12,6 +13,7 @@ class Battle:
 
     def __init__(self, team_size=6):
         self.get_team(team_size=team_size)
+        self.enemy_team = None
 
     def get_team(self, team_size=6):
         """
@@ -63,38 +65,6 @@ class Battle:
         else:
             raise NotImplementedError
 
-    def apply_move(self, attacking_pokemon, defending_pokemon, move):
-        """
-        Apply the move to the enemy pokemon
-        """
-        move_damage = attacking_pokemon.moveset[move].stats["power"]
-        if move_damage is None:
-            move_damage = 0
-        move_accuracy = attacking_pokemon.moveset[move].stats["accuracy"]
-        if move_accuracy is None:
-            move_accuracy = 100
-        move_type = attacking_pokemon.moveset[move].stats["type"]
-        if random.random() < (move_accuracy / 100):
-            defending_pokemon.health_points = (
-                defending_pokemon.health_points - move_damage
-            )
-            print(
-                f"{attacking_pokemon} used {attacking_pokemon.moveset[move]} on {defending_pokemon} and did {move_damage} damage"
-            )
-        else:
-            print(
-                f"{attacking_pokemon} used {attacking_pokemon.moveset[move]} on {defending_pokemon} but it missed"
-            )
-
-        if defending_pokemon.health_points <= 0:
-            time.sleep(1.5)
-            print("\n")
-            print(f"{defending_pokemon} fainted")
-            defending_pokemon.health_points = 0
-            defending_pokemon.fainted = True
-
-        return defending_pokemon.health_points
-
     def start_battle(self):
         """
         Start the battle
@@ -121,10 +91,7 @@ class Battle:
         player_pokemon = self.team[0]
         enemy_pokemon = self.enemy_team[0]
 
-        if player_pokemon.stats["speed"] >= enemy_pokemon.stats["speed"]:
-            player_turn = True
-        else:
-            player_turn = False
+        player_turn = player_pokemon.stats["speed"] >= enemy_pokemon.stats["speed"]
 
         while len(player_remaining_pokemon) > 0 and len(enemy_remaining_pokemon) > 0:
             print("\n")
@@ -133,52 +100,13 @@ class Battle:
             time.sleep(2)
             print("\n")
             if player_turn:
-                print(player_pokemon, "'s turn:")
-                time.sleep(1)
-                for move in range(len(player_pokemon.moveset)):
-                    print(move + 1, ": ", player_pokemon.moveset[move].name)
-                print("\n")
-                # TODO: Make sure player move is between 1 and 4
-                player_move = int(input("Choose your move [1-4]: "))
-                player_move -= 1
-                print("\n")
-                time.sleep(1)
-                self.apply_move(player_pokemon, enemy_pokemon, player_move)
-                time.sleep(2)
-                print("\n")
-                if enemy_pokemon.health_points <= 0:
-                    enemy_remaining_pokemon.remove(enemy_pokemon)
-                    if len(enemy_remaining_pokemon) > 0:
-                        enemy_pokemon = enemy_remaining_pokemon[0]
-                        print(f"Enemy chooses {enemy_pokemon}")
-                        time.sleep(1)
-                        print("\n")
-                        print(player_pokemon, " VS ", enemy_pokemon)
-                        time.sleep(1)
-                    else:
-                        enemy_pokemon = None
+                enemy_pokemon, enemy_remaining_pokemon = player_turn_logic(
+                    player_pokemon, enemy_pokemon, enemy_remaining_pokemon
+                )
             else:
-                print(enemy_pokemon, "'s turn:")
-                time.sleep(1)
-                enemy_move = random.randint(0, len(enemy_pokemon.moveset) - 1)
-                print("\n")
-                self.apply_move(enemy_pokemon, player_pokemon, enemy_move)
-                print("\n")
-                if player_pokemon.health_points <= 0:
-                    player_remaining_pokemon.remove(player_pokemon)
-                    if len(player_remaining_pokemon) > 0:
-                        print("Which pokemon do you choose?")
-                        time.sleep(0.7)
-                        for i, poke in enumerate(player_remaining_pokemon):
-                            print(i + 1, ": ", poke)
-                            time.sleep(0.3)
-                        poke_choice = int(input("Choose a pokemon: "))
-                        poke_choice -= 1
-                        player_pokemon = player_remaining_pokemon[poke_choice]
-                        print(player_pokemon, " VS ", enemy_pokemon)
-                        time.sleep(1)
-                    else:
-                        player_pokemon = None
+                player_pokemon, player_remaining_pokemon = enemy_turn_logic(
+                    player_pokemon, enemy_pokemon, player_remaining_pokemon
+                )
             player_turn = not player_turn
 
         if len(player_remaining_pokemon) > 0:
